ecf42ccf7d32f8294f1bc5852d5d4d35e1ba81b8
LSH <tmdghksgkqru@naver.com>
2026-01-06 01:42:13 +0900

    test2

--- myreactdocker/src/App.jsx
+++ myreactdocker/src/App.jsx
@@ -29,8 +29,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={testNodeConn}>
+          testNodeConn
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
