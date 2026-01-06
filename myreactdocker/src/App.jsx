94c97578ca9e55c4ca0061684fcbf081335fecf3
LSH <tmdghksgkqru@naver.com>
2026-01-06 12:04:32 +0900

    react node fetch

--- myreactdocker/src/App.jsx
+++ myreactdocker/src/App.jsx
@@ -6,6 +6,7 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+
   const connNode = () =>{
     fetch('http://10.0.20.6:3001')
     .then(()=>{
@@ -28,13 +29,11 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-<<<<<<< HEAD
+
         <button onClick={connNode}>
           React-Node-NaverCloud
-=======
-        <button onClick={testNodeConn}>
-          testNodeConn
->>>>>>> c1bf8ded2b393ecfc8ceef1417f863791a5375da
+
+        
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
